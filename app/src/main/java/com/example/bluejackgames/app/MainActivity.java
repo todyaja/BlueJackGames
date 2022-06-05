73d8c6f40a9914a45d7dd03a2d25074f6e62d237
Tody <todyprasetya@gmail.com>
2022-06-05 13:00:19 +0700

    create all empty activity

---
+++ app/src/main/java/com/example/bluejackgames/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.bluejackgames.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bluejackgames.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/bluejackgames/app/ProductPageActivity.java
@@ -0,0 +1,16 @@
+package com.example.bluejackgames.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bluejackgames.R;
+
+public class ProductPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_page);
+    }
+}
