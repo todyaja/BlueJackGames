6fbc22f2ad2bb1acd2c72ce9e5aada51f78cd74f
WilliamHalim717 <76192297+WilliamHalim717@users.noreply.github.com>
2022-06-05 14:55:09 +0800

    create home page

--- app/src/main/java/com/example/bluejackgames/app/HomePageActivity.java
+++ app/src/main/java/com/example/bluejackgames/app/HomePageActivity.java
@@ -2,15 +2,50 @@ package com.example.bluejackgames.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.bluejackgames.R;
 
 public class HomePageActivity extends AppCompatActivity {
 
+    TextView product, about, logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        setTitle("Welcome, ");
+
+        product = findViewById(R.id.product_page);
+        about = findViewById(R.id.about_page);
+        logout = findViewById(R.id.logout_page);
+
+        product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePageActivity.this, ProductPageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePageActivity.this, AboutPageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePageActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
